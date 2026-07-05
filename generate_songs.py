c47907c5c39982cff7fb470b29ac05a37a1c337d
Ku1oE <aira3944@gmail.com>
2026-07-05 22:45:11 +0900

    generate_songs.py written

--- generate_songs.py
+++ generate_songs.py
@@ -20,12 +20,21 @@ def generate_tags(tags):
     return ''.join(f'<span class="tag">{tag}</span>' for tag in tags)
 
 
+
 def main():
     df = pd.read_csv('data.csv')
+    with open('songs-template.html', 'r', encoding='utf-8') as f:
+        template_content = f.read()
+    songs_html_list = []
     for _, row in df.iterrows():
-        tags = [tag.strip() for tag in row['Tags'].split(",")]
-        tag_html = generate_tags(tags)
-        generate_song_html(row)
+        songs_html = generate_song_html(row)
+        songs_html_list.append(songs_html)
 
+    all_songs_combined = "\n".join(songs_html_list)
 
-    
+    final_content = template_content.replace('{{songs}}', all_songs_combined)
+
+    with open('output.html', 'w', encoding='utf-8') as f:
+        f.write(final_content)
+
+    print("success")
